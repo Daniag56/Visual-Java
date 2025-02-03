e14b0bfded133fe42758855ab77a49692e891633
daniag56 <dnlrsgrc@gmail.com>
2025-02-03 20:25:47 +0100

    mensaje

--- EjercicioHashMap/src/GestionHash.java
+++ EjercicioHashMap/src/GestionHash.java
@@ -9,22 +9,20 @@ public class GestionHash {
     public void agregar(String ciudad, int codPostal) {
         ciudad = "";
         codPostal = 0;
-            if (!map.containsKey(ciudad)) {
-                ArrayList<Integer> listamap = new ArrayList<>();
+        if (!map.containsKey(ciudad)) {
+            ArrayList<Integer> listamap = new ArrayList<>();
+            listamap.add(codPostal);
+            map.put(ciudad, listamap);
+
+        } else {
+            ArrayList<Integer> listamap = map.get(ciudad);
+            if (!listamap.contains(codPostal)) {
                 listamap.add(codPostal);
-                map.put(ciudad, listamap);
-                
-            } else {
-                ArrayList<Integer> listamap = map.get(ciudad);
-                if (!listamap.contains(codPostal)) {
-                    listamap.add(codPostal);
-                }
             }
         }
-        
-    
+    }
 
-    public boolean borrar(String ciudad) {  
+    public boolean borrar(String ciudad) {
         if (map.containsKey(ciudad)) {
             map.remove(ciudad);
             return true;
@@ -33,19 +31,22 @@ public class GestionHash {
     }
 
     public void mostrar() {
-        for (Map.Entry<String,ArrayList<Integer>> mapeable : map.entrySet()) {
-            System.out.println(mapeable);
+        for (Map.Entry<String, ArrayList<Integer>> mapeable : map.entrySet()) {
+            System.out.print("La provincia: " + mapeable.getKey() + " tiene los codigos postales: ");
+            for (int i = 0; i < mapeable.getValue().size(); i++) {
+                System.out.println(mapeable.getValue().get(i));
+            }
         }
     }
-    public void MostrarCpProvincia(String ciudad){
+
+    public void MostrarCpProvincia(String ciudad) {
         if (map.containsKey(ciudad)) {
-            ArrayList<Integer>listamap = map.get(ciudad);
-            System.out.print("Provincia: "+ciudad+"tiene los cp:");
+            ArrayList<Integer> listamap = map.get(ciudad);
+            System.out.print("Provincia: " + ciudad + "tiene los cp:");
             for (Integer miCp : listamap) {
                 System.out.print(miCp + "");
             }
             System.out.println();
         }
     }
-    }
-
+}
