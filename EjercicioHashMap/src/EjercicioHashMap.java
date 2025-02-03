97abe84b48eda57ad53f97122d5fe66871b62d1f
daniag56 <dnlrsgrc@gmail.com>
2025-02-03 20:04:35 +0100

    mensaje

--- EjercicioHashMap/src/EjercicioHashMap.java
+++ EjercicioHashMap/src/EjercicioHashMap.java
@@ -3,26 +3,43 @@ import java.util.Scanner;
 public class EjercicioHashMap {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
+        GestionHash gestionar = new GestionHash();
+        String ciudad = "";
+        int codPostal = 0;
 
-        System.out.println("Introduzca la opcion para iterar: ");
-        System.out.print("1. Introducir datos en el HashMap");
-        System.out.print("2. Mostrar datos del HashMap");
-        int opcion = sc.nextInt();
-        GestionHash gestion = new GestionHash();
+        int opcion;
 
-        switch (opcion) {
-            case 1:
-                gestion.agregar("Malaga",2452);
-                break;
-            case 2:
-                
-                break;
-            default:
-                System.out.println("Introduzca una opcion valida");
-                break;
-        }
+        do {
+            System.out.println("Introduzca la opcion para iterar: ");
+            System.out.println("1. Introducir datos en el HashMap");
+            System.out.println("2. Mostrar datos del HashMap");
+            System.out.println("3. Borrado");
+            System.out.println("4. Salir");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduzca la ciudad: ");
+                    ciudad = sc.nextLine();
+                    System.out.println("Introduzca el codigo postal de la ciudad: ");
+                    codPostal = sc.nextInt();
+                    sc.nextLine();
+                    gestionar.agregar(ciudad, codPostal);
+                    break;
+                case 2:
+                    gestionar.mostrar();
+                    break;
+                case 3:
+                    System.out.println("Introduzca la ciudad a borrar: ");
+                    ciudad = sc.nextLine();
+                    gestionar.borrar(ciudad);
+                default:
+                    System.out.println("Adios");
+                    break;
+            }
+        } while (opcion != 4);
 
         sc.close();
     }
+
 }
--- EjercicioHashMap/src/GestionHash.java
+++ EjercicioHashMap/src/GestionHash.java
@@ -1,26 +1,51 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.Map;
 
 public class GestionHash {
+
     HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
 
     public void agregar(String ciudad, int codPostal) {
-        if (map.containsKey(ciudad)) {
-            ArrayList<Integer> listamap;
-            listamap = map.get(ciudad);
-            if (!listamap.contains(codPostal)) {
+        ciudad = "";
+        codPostal = 0;
+            if (!map.containsKey(ciudad)) {
+                ArrayList<Integer> listamap = new ArrayList<>();
                 listamap.add(codPostal);
+                map.put(ciudad, listamap);
+                
+            } else {
+                ArrayList<Integer> listamap = map.get(ciudad);
+                if (!listamap.contains(codPostal)) {
+                    listamap.add(codPostal);
+                }
             }
-        }else{
-            ArrayList<Integer> listamap = new ArrayList<Integer>();
-            map.put(ciudad, listamap);
+        }
+        
+    
+
+    public boolean borrar(String ciudad) {  
+        if (map.containsKey(ciudad)) {
+            map.remove(ciudad);
+            return true;
+        }
+        return false;
+    }
+
+    public void mostrar() {
+        for (Map.Entry<String,ArrayList<Integer>> mapeable : map.entrySet()) {
+            System.out.println(mapeable);
         }
     }
-
-    public void mostrar(){
-        
+    public void MostrarCpProvincia(String ciudad){
+        if (map.containsKey(ciudad)) {
+            ArrayList<Integer>listamap = map.get(ciudad);
+            System.out.print("Provincia: "+ciudad+"tiene los cp:");
+            for (Integer miCp : listamap) {
+                System.out.print(miCp + "");
+            }
+            System.out.println();
+        }
+    }
     }
 
-    
-}
